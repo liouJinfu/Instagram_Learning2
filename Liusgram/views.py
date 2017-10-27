3ddd18bf5952672a6b50582b9e30ba5e0189d5e1
Liu jinfu <opp1992@qq.com>
2017-10-27 22:28:01 +0800

    class 3

--- Liusgram/views.py
+++ Liusgram/views.py
@@ -2,11 +2,14 @@
 
 from Liusgram import app
 from models import Image, User, Comment
-from flask import render_template, redirect
+from flask import render_template, redirect, request
+
+
 @app.route('/')
 def index():
     images = Image.query.order_by('id desc').limit(10).all()
     return render_template('index.html', images = images)
+
 @app.route('/image/<int:image_id>/')
 def image(image_id):
     image = Image.query.get(image_id)
@@ -14,6 +17,7 @@ def image(image_id):
         return redirect('/')
     else:
         return render_template('pageDetail.html', image = image)
+
 @app.route('/profile/<int:user_id>')
 def user(user_id):
     user = User.query.get(user_id)
@@ -21,3 +25,14 @@ def user(user_id):
         return redirect('/')
     else :
         return render_template('profile.html', user = user)
+
+@app.route('/loginpage/')
+def login():
+    return render_template('login.html')
+
+@app.route('/register/')
+def register():
+    #request.args
+    #request.form
+    username = request.values.get('username').strip()
+    password = request.values.get('password').strip()
