8cfb9b260fe7b37a6d794ff1af2230bea2f176a6
Liu jinfu <opp1992@qq.com>
2017-10-27 21:46:39 +0800

    add the add index.html and profile.html and image.html

--- Liusgram/views.py
+++ Liusgram/views.py
@@ -1,7 +1,23 @@
 #-*- encoding=UTF-8 -*-
 
 from Liusgram import app
-
+from models import Image, User, Comment
+from flask import render_template, redirect
 @app.route('/')
 def index():
-    return 'hello'
+    images = Image.query.order_by('id desc').limit(10).all()
+    return render_template('index.html', images = images)
+@app.route('/image/<int:image_id>/')
+def image(image_id):
+    image = Image.query.get(image_id)
+    if image == None :
+        return redirect('/')
+    else:
+        return render_template('pageDetail.html', image = image)
+@app.route('/profile/<int:user_id>')
+def user(user_id):
+    user = User.query.get(user_id)
+    if user == None :
+        return redirect('/')
+    else :
+        return render_template('profile.html', user = user)
--- manager.py
+++ manager.py
@@ -8,11 +8,13 @@ from sqlalchemy import or_, and_
 manager = Manager(app)
 
 def getImageUrl():
-    return 'http://images.nowcoder.com/head/' + str(random.randint(0, 1000)) + 't.png'
+    return 'http://images.nowcoder.com/head/' + str(random.randint(0, 1000)) + 'm.png'
 @manager.command
 def init_database():
     db.drop_all()
+    ##创建表
     db.create_all()
+    ##Insert 插入数据
     for i in range(0, 100):
         db.session.add(User('user'+ str(i+1), 'a' + str(i+1)))
         for j in range(0, 3):
@@ -20,6 +22,8 @@ def init_database():
             for k in range(0, 3):
                 db.session.add(Comment('comments'+str(k),  1+3*i+j, i+1))
     db.session.commit()
+
+    ###查询
     print 1, User.query.all()
     print 2, User.query.get(3)
     print 3, User.query.filter_by(id = 5).first()
@@ -31,9 +35,23 @@ def init_database():
     print 8, user.images.all()
 
     image = Image.query.get(1)
-    print 11, image.user
+    print 9, image.user
 
+    ##更新
+    for i in range(50, 100, 1):
+        user = User.query.get(i)
+        user.username = '[New]' + user.username
+    db.session.commit()
+    User.query.filter_by(id = 1).update({'username' : '[New2]'})
+    db.session.commit()
 
+    ## 删除
+    # for i in range(5, 100):
+    #     comment = Comment.query.get(i)
+    #     db.session.delete(comment)
+    # db.session.commit()
+    # Comment.query.filter(id > 50).delete()
+    # db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
